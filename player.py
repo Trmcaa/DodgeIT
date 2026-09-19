0d04c754048d3a297e42326b48a5087f25d6f374
hracisko <jtrmik@gmail.com>
2026-09-19 23:00:38 +0200

    Second commit

--- player.py
+++ player.py
@@ -5,3 +5,10 @@ class Player():
     def __init__(self):
         self.rect = pg.Rect(settings.STARTING_POS_W, settings.STARTING_POS_H, settings.PLAYER_W, settings.PLAYER_H)
         
+    def handle_input(self, keys):
+        self.keys = keys
+        self.keys = pg.key.get_pressed()
+        if self.keys == pg.K_a:
+            self.rect.x -= settings.PLAYER_SPEED
+        if self.keys == pg.K_d:
+            self.rect.x += settings.PLAYER_SPEED
